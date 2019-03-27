3756c6221e97adad65b731e08ce2703dc5d9566b
NaokiTokiyama <naokitokiyama@NaokiTokiyamanoMacBook-Pro.local>
2019-03-28 01:42:19 +0900

    Added Blog Application

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,12 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:100]
+
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
     def __str__(self):
-        return self.title
+        return self.title[:20]
