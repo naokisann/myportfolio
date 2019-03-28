6cc0c93e9db35fee8cb14afc2f568a5c7d281eca
NaokiTokiyama <naokitokiyama@NaokiTokiyamanoMacBook-Pro.local>
2019-03-28 21:32:57 +0900

    画像がなくても

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/',blank=True)
 
     def summary(self):
         return self.body[:100]
